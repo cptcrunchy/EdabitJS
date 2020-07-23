c7c07c19dcc89a760c3c40461e03ad744f8bf02d
Jason Gutierrez <jasong84@gmail.com>
2020-07-23 17:06:57 -0500

    removed inline test code

--- fiscalCode.js
+++ fiscalCode.js
@@ -7,18 +7,10 @@ function fiscalCode(person) {
 	
 	let sNameCode = partial(person.surname)
 	let [first,_,third, fourth] = getConsonants(person.name)
-	let fNameCode = (getConsonants(person.name).length > 3) ? [first, third, fourth].join('') : partial(person.name)//?
-	let genderCode = (person.gender === "M") ? "0".concat(day).slice(-2) : +day + 40 //?
+	let fNameCode = (getConsonants(person.name).length > 3) ? [first, third, fourth].join('') : partial(person.name)
+	let genderCode = (person.gender === "M") ? "0".concat(day).slice(-2) : +day + 40
 
 	return [sNameCode, fNameCode, year.slice(-2), months[month], genderCode].join('')
 }
 
-person1 = { name: "Helen", surname: "Yu", gender: "F", dob: "1/12/1950" }
-person2 = { name: "Al", surname: "Capone", gender: "M", dob: "17/1/1899" }
-
-fiscalCode(person1)//?
-                    //  YUXHLN50T41
-fiscalCode(person2)//?
-                    //  CPNLAX99A17
-
-// module.exports = fiscalCode;
+module.exports = fiscalCode;
