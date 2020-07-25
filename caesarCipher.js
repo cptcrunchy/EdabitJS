9a3ac9603379b706f392d7c598d54b6c8d7ca01a
Jason Gutierrez <jasong84@gmail.com>
2020-07-25 17:22:33 -0500

    Refactored Ceasar Cipher

--- caesarCipher.js
+++ caesarCipher.js
@@ -1,19 +1,8 @@
 function caesarCipher(s, k) {
-    const isUpperCase = s => s === s.toUpperCase()
-    const isLetter = s => (s.charCodeAt(s) >= 65 && s.charCodeAt(s) <= 90) || (s.charCodeAt(s) >= 97 && s.charCodeAt(s) <= 122)   
-    
-    return Array.from(s).map( l => {
-        let codex = ""
-        if (isLetter(l)) {
-            let delta = isUpperCase(l) ? 65 : 97
-            codex += String.fromCharCode((l.charCodeAt(l) + k - delta) % 26 + delta)
-        } else { 
-            codex += l
-        }
-        return codex
-    }).join('')
+    return s.replace(/[a-z]/gi,  c => {
+        const d = c < 'a' ? 65 : 97
+        return String.fromCharCode((c.charCodeAt(0) - d + k) % 26 + d)
+    })
 }
-
-// caesarCipher("One should not worry over things that have already happened and that cannot be changed.", 49)//?
 
 module.exports = caesarCipher
